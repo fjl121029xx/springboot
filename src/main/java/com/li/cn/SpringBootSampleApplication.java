52d69ae48cd57ad354eec64a8258f49a95f84b83
fjl121029xx <sishengqikuo_xx@163.com>
2018-01-02 17:03:23 +0800

    spring boot 05

--- src/main/java/com/li/cn/SpringBootSampleApplication.java
+++ src/main/java/com/li/cn/SpringBootSampleApplication.java
@@ -1,15 +1,26 @@
 package com.li.cn;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableCaching
+@RestController
+@EnableAutoConfiguration
 public class SpringBootSampleApplication {
+
+    @RequestMapping("/")
+    String home() {
+
+        return "Hello World";
+    }
 
     public static void main(String[] args) {
 
-        SpringApplication.run(SpringBootSampleApplication.class,args);
+        SpringApplication.run(SpringBootSampleApplication.class, args);
     }
 }

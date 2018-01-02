29417f74eb82a121e4b8d0f5b10cc9a7800a898d
fjl121029xx <sishengqikuo_xx@163.com>
2018-01-02 16:59:30 +0800

    spring boot 05

--- src/main/java/com/li/cn/controller/HelloController.java
+++ src/main/java/com/li/cn/controller/HelloController.java
@@ -15,35 +15,35 @@ import java.util.Map;
 @RequestMapping("/hello")
 public class HelloController {
     /**
-     *
      * @return
      */
     @RequestMapping
-    public String hello(){
+    public String hello() {
+
         return "Hello spring-boot";
     }
 
     @RequestMapping("/map")
-    public Map<String,Object> map(){
-        Map<String,Object> map = new HashMap<>();
-        map.put("name","张三丰");
-        map.put("age",17);
+    public Map<String, Object> map() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "张三丰");
+        map.put("age", 17);
 
         return map;
     }
 
     @RequestMapping("/list")
-    public List<Object> list(){
+    public List<Object> list() {
         List<Object> l = new ArrayList<>();
         l.add("捻花功");
         l.add("无妄神功");
 
-        return  l;
+        return l;
     }
 
     @RequestMapping("/findStus")
     @ResponseBody
-    public Object findStus(){
+    public Object findStus() {
         List<Student> stus = new ArrayList<>();
         Student s = new Student();
         s.setId(1);
